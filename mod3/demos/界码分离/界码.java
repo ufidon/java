3f0ff7f28eb89ce635f1453e167977e4a61c3f25
ufidon <ufidon@gmail.com>
2023-02-04 08:35:17 -0500

    javafx url

--- mod3/demos/界码分离/界码.java
+++ mod3/demos/界码分离/界码.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,8 +11,15 @@ public class 界码 extends Application {
     
     @Override
     public void start(Stage 台) throws Exception {
-        Parent 根 = FXMLLoader.load((new File("登.fxml")).toURI().toURL());
+        //Parent 根 = FXMLLoader.load((new File("登.fxml")).toURI().toURL());
+        Parent 根 = FXMLLoader.load(getClass().getResource("登.fxml"));
+        // Parent 根 = FXMLLoader.load(new URL("file:///home/x/scratch/java/界码分离/登.fxml"));
+        // Parent 根 = FXMLLoader.load(new URL("file://localhost/home/x/scratch/java/界码分离/登.fxml"));
+
         
+        // https://en.wikipedia.org/wiki/File_URI_scheme
+        //https://stackoverflow.com/questions/126141/how-do-you-find-out-which-version-of-gtk-is-installed-on-ubuntu
+         
         台.setTitle("面码分离");
         台.setScene(new Scene(根, 300, 250));
         台.getIcons().add(new Image("城门.png"));
